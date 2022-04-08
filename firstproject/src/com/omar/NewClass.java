2f4778632cd29b244a727f974257ccecc5b821be
Omar Al-Ezzi <ishtaar.it@gmail.com>
2022-04-08 18:11:46 +0200

    first run

--- firstproject/src/com/omar/NewClass.java
+++ firstproject/src/com/omar/NewClass.java
@@ -3,7 +3,10 @@ package com.omar;
 public class NewClass {
 
 		public static void main(String args[]) {
-			System.out.print("Hello");
+			System.out.print("Hello");  
+			//System.out.print(5/0); run time error example
+			//System.out.print(5+1/2); logical error example (priority from the developer)  
+
 		}
 		
 }
