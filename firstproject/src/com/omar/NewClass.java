f8bbd3bb1fb8c1a585c65b702357e88295d0f2db
Omar Al-Ezzi <ishtaar.it@gmail.com>
2022-04-08 18:33:33 +0200

    first run

--- firstproject/src/com/omar/NewClass.java
+++ firstproject/src/com/omar/NewClass.java
@@ -6,7 +6,20 @@ public class NewClass {
 			System.out.print("Hello");  
 			//System.out.print(5/0); run time error example
 			//System.out.print(5+1/2); logical error example (priority from the developer)  
-
+			System.out.println("1Omar Al-Ezzi");  
+			System.out.println("2Omar\nAl-Ezzi");  
+			System.out.println("3Omar\n\nAl-Ezzi");  
+			System.out.println("4Omar\tAl-Ezzi");  
+			System.out.println("5Omar\bAl-Ezzi");  
+			System.out.println("6Omar\b\b\nAl-Ezzi");  
+			System.out.println("7Omar\n\b\bAl-Ezzi");  
+			System.out.println("8Omar \"Al-Ezzi\"");  
+			System.out.println("9Omar 'Al-Ezzi'");  
+			System.out.println("10Omar \\Al-Ezzi\\");  
+					
+			System.out.println("A\nd\bJAVA\n"); 
+			//syso + ctrl +space
+			
 		}
 		
 }
